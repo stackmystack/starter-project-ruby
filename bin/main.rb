e5e0812049400cce62c99a3a3c4aaee78067bede
Firas al-Khalil <firasalkhalil@gmail.com>
2024-09-12 11:58:48 +0200

    lib: load automatically in tests and main

--- bin/main.rb
+++ bin/main.rb
@@ -1,6 +1,11 @@
 #!/usr/bin/env ruby
 # frozen_string_literal: true
 
+lib = File.expand_path('lib', __dir__)
+$LOAD_PATH.unshift(lib) if !$LOAD_PATH.include?(lib)
+
+require 'your_gem_name'
+
 puts 'Hello, world!'
 puts 'Args:'
 puts ARGV.inspect
--- test/lib.rb
+++ test/lib.rb
@@ -1,9 +1,14 @@
 # frozen_string_literal: true
 
+lib = File.expand_path('lib', __dir__)
+$LOAD_PATH.unshift(lib) if !$LOAD_PATH.include?(lib)
+
 require 'bundler/setup'
 # require 'debug'
 require 'minitest/autorun'
 require 'minitest/focus'
 require 'minitest/reporters'
+
+require 'your_gem_name'
 
 Minitest::Reporters.use!
